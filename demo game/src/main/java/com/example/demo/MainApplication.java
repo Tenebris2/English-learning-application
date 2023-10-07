238c8d059d07c5a895e6dccc7bf18b241fb963f5
ShinyAri <113637708+ShinyAri@users.noreply.github.com>
2023-10-07 08:07:18 +0700

    demo game

---
+++ demo game/src/main/java/com/example/demo/MainApplication.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class MainApplication extends Application {
+
+    public static final ArrayList<String> winningWords = new ArrayList<>();
+    public static final ArrayList<String> dictionaryWords = new ArrayList<>();
+
+    private static Stage stageReference;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        initializeWordLists();
+        stageReference = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("wordle.fxml"));
+        Parent root = fxmlLoader.load();
+
+        MainController mainController = fxmlLoader.getController();
+        mainController.createUI();
+        mainController.getRandomWord();
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+        Scene scene = new Scene(root, 500, 720);
+        stage.setMinWidth(500);
+        stage.setMinHeight(720);
+        stage.setMaxWidth(screenWidth);
+        stage.setMaxHeight(screenHeight);
+        stage.setTitle("Wordle");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/wordle.png"))));
+        stage.setScene(scene);
+        stage.show();
+
+        mainController.gridRequestFocus();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public static void quit() {
+        stageReference.close();
+    }
+
+    public void initializeWordLists() {
+        InputStream winning_words = getClass().getResourceAsStream("winning-words.txt");
+        InputStream dictionary = getClass().getResourceAsStream("dictionary.txt");
+
+        if (winning_words != null && dictionary != null) {
+            Stream<String> winning_words_lines = new BufferedReader(new InputStreamReader(winning_words)).lines();
+            winning_words_lines.forEach(winningWords::add);
+            Stream<String> dictionary_lines = new BufferedReader(new InputStreamReader(dictionary)).lines();
+            dictionary_lines.forEach(dictionaryWords::add);
+        } else
+            quit();
+    }
+}
---
+++ demo game/src/main/java/com/example/demo/MainFunction.java
@@ -0,0 +1,186 @@
+package com.example.demo;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+import java.util.*;
+
+import static com.example.demo.MainApplication.winningWords;
+
+public class MainFunction {
+
+    private static MainFunction INSTANCE = null;
+
+    private final String[] firstRowLetters = {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"};
+    private final String[] secondRowLetters = {"A", "S", "D", "F", "G", "H", "J", "K", "L"};
+    private final String[] thirdRowLetters = { "Z", "X", "C", "V", "B", "N", "M"};
+
+    private int CURRENT_ROW = 1;
+    private int CURRENT_COLUMN = 1;
+    private final int MAX_COLUMN = 5;
+    private final int MAX_ROW = 6;
+    private String winningWord;
+
+    private MainFunction() {
+    }
+
+    public static MainFunction getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new MainFunction();
+        return INSTANCE;
+    }
+
+    public void createTitleHBox(HBox titleHBox) {
+            ArrayList<Label> titleLetters = new ArrayList<>();
+            for (String letter : new String[]{"W", "O", "R", "D", "L", "E"}) {
+                Label label = new Label(letter);
+                label.getStyleClass().add("title-letter"); // Add CSS class to the label
+                titleLetters.add(label);
+            }
+            for (Label label : titleLetters)
+                titleHBox.getChildren().add(label);
+    }
+
+    public void createGrid(GridPane gridPane) {
+        for (int i = 1; i <= MAX_ROW; i++) {
+            for (int j = 1; j <= MAX_COLUMN; j++) {
+                Label label = new Label();
+                label.getStyleClass().add("default-tile");
+                gridPane.add(label, j, i);
+            }
+        }
+    }
+
+    public void createKeyboard(GridPane keyboardRow1, GridPane keyboardRow2, GridPane keyboardRow3) {
+        for (int i = 0; i < firstRowLetters.length; i++) {
+            Label label = new Label();
+            label.getStyleClass().add("keyboardTile");
+            label.setText(firstRowLetters[i]);
+            keyboardRow1.add(label, i, 1);
+        }
+        for (int i = 0; i < secondRowLetters.length; i++) {
+            Label label = new Label();
+            label.getStyleClass().add("keyboardTile");
+            label.setText(secondRowLetters[i]);
+            keyboardRow2.add(label, i, 2);
+        }
+        for (int i = 0; i < thirdRowLetters.length; i++) {
+            Label label = new Label();
+            label.getStyleClass().add("keyboardTile");
+            label.setText(thirdRowLetters[i]);
+            keyboardRow3.add(label, i, 3);
+        }
+    }
+
+    private void setLabelText(GridPane gridPane, int searchRow, int searchColumn, String input) {
+        Label label = getLabel(gridPane, searchRow, searchColumn);
+        if (label != null)
+            label.setText(input.toUpperCase());
+    }
+
+    private Label getLabel(GridPane gridPane, int searchRow, int searchColumn) {
+        for (Node child : gridPane.getChildren()) {
+            Integer r = GridPane.getRowIndex(child);
+            Integer c = GridPane.getColumnIndex(child);
+            int row = r == null ? 0 : r;
+            int column = c == null ? 0 : c;
+            if (row == searchRow && column == searchColumn && (child instanceof Label))
+                return (Label) child;
+        }
+        return null;
+    }
+
+    private Label getLabel(GridPane gridPane, String letter) {
+        Label label;
+        for (Node child : gridPane.getChildren()) {
+            if (child instanceof Label) {
+                label = (Label) child;
+                if (letter.equalsIgnoreCase(label.getText()))
+                    return label;
+            }
+        }
+        return null;
+    }
+
+    private String getLabelText(GridPane gridPane, int searchRow, int searchColumn) {
+        Label label = getLabel(gridPane, searchRow, searchColumn);
+        if (label != null)
+            return label.getText().toLowerCase();
+        return null;
+    }
+
+    private void setLabelStyleClass(GridPane gridPane, int searchRow, int searchColumn, String styleclass) {
+        Label label = getLabel(gridPane, searchRow, searchColumn);
+        if (label != null) {
+            label.getStyleClass().add(styleclass);
+        }
+    }
+
+    private void clearLabelStyleClass(GridPane gridPane, int searchRow, int searchColumn) {
+        Label label = getLabel(gridPane, searchRow, searchColumn);
+        if (label != null) {
+            label.getStyleClass().clear();
+        }
+    }
+
+    private String getWordFromCurrentRow(GridPane gridPane) {
+        StringBuilder input = new StringBuilder();
+        for (int j = 1; j <= MAX_COLUMN; j++)
+            input.append(getLabelText(gridPane, CURRENT_ROW, j));
+        return input.toString();
+    }
+
+    public void onKeyPressed(GridPane gridPane, GridPane keyboardRow1, GridPane keyboardRow2,
+                             GridPane keyboardRow3, KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
+            onBackspacePressed(gridPane);
+        } else if (keyEvent.getCode().isLetterKey()) {
+            onLetterPressed(gridPane, keyEvent);
+        }
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            onEnterPressed(gridPane, keyboardRow1, keyboardRow2, keyboardRow3);
+        }
+    }
+
+    private void onBackspacePressed(GridPane gridPane) {
+        if ((CURRENT_COLUMN == MAX_COLUMN || CURRENT_COLUMN == 1)
+                && !Objects.equals(getLabelText(gridPane, CURRENT_ROW, CURRENT_COLUMN), "")) {
+            setLabelText(gridPane, CURRENT_ROW, CURRENT_COLUMN, "");
+            clearLabelStyleClass(gridPane, CURRENT_ROW, CURRENT_COLUMN);
+            setLabelStyleClass(gridPane, CURRENT_ROW, CURRENT_COLUMN, "default-tile");
+        } else if ((CURRENT_COLUMN > 1 && CURRENT_COLUMN < MAX_COLUMN)
+                || (CURRENT_COLUMN == MAX_COLUMN && Objects.equals(getLabelText(gridPane, CURRENT_ROW, CURRENT_COLUMN), ""))) {
+            CURRENT_COLUMN--;
+            setLabelText(gridPane, CURRENT_ROW, CURRENT_COLUMN, "");
+            clearLabelStyleClass(gridPane, CURRENT_ROW, CURRENT_COLUMN);
+            setLabelStyleClass(gridPane, CURRENT_ROW, CURRENT_COLUMN, "default-tile");
+        }
+    }
+
+    private void onLetterPressed(GridPane gridPane, KeyEvent keyEvent) {
+        if (Objects.equals(getLabelText(gridPane, CURRENT_ROW, CURRENT_COLUMN), "")) {
+            setLabelText(gridPane, CURRENT_ROW, CURRENT_COLUMN, keyEvent.getText());
+            Label label = getLabel(gridPane, CURRENT_ROW, CURRENT_COLUMN);
+            setLabelStyleClass(gridPane, CURRENT_ROW, CURRENT_COLUMN, "tile-with-letter");
+            if (CURRENT_COLUMN < MAX_COLUMN)
+                CURRENT_COLUMN++;
+        }
+    }
+
+    private void onEnterPressed(GridPane gridPane, GridPane keyboardRow1, GridPane keyboardRow2,
+                                GridPane keyboardRow3) {
+        if (CURRENT_ROW <= MAX_ROW && CURRENT_COLUMN == MAX_COLUMN) {
+            String guess = getWordFromCurrentRow(gridPane).toLowerCase();
+            CURRENT_ROW++;
+            CURRENT_COLUMN = 1;
+        }
+    }
+
+    public void getRandomWord() {
+        winningWord = winningWords.get(new Random().nextInt(winningWords.size()));
+    }
+}
