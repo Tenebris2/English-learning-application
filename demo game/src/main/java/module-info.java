252ed8ac39c6edcc31be0890503cc88daa2015cf
ShinyAri <113637708+ShinyAri@users.noreply.github.com>
2023-10-08 14:03:35 +0700

    Add files via upload
    them cac chuc nang co ban cua game

--- demo game/src/main/java/module-info.java
+++ demo game/src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module com.example.demo {
+module com.example.demo2 {
     requires javafx.controls;
     requires javafx.fxml;
-            
+
     requires org.kordamp.bootstrapfx.core;
-            
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+
+    opens com.example.demo2 to javafx.fxml;
+    exports com.example.demo2;
 }
