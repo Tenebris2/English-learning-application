8d09bacefdb77d390302f746de0db2268c71c5c2
ShinyAri <113637708+ShinyAri@users.noreply.github.com>
2023-10-10 16:39:07 +0700

    Delete game2 directory

--- game2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.game2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.game2 to javafx.fxml;
-    exports com.example.game2;
-}
