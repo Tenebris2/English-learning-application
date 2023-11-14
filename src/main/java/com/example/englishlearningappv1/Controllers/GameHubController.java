e6a35369ec2d974e2bf768655f887879047c8b2a
ShinyAri <tanhngusi39@gmail.com>
2023-11-14 19:48:07 +0700

    upgrade design

--- src/main/java/com/example/englishlearningappv1/Controllers/GameHubController.java
+++ src/main/java/com/example/englishlearningappv1/Controllers/GameHubController.java
@@ -4,6 +4,7 @@ package com.example.englishlearningappv1.Controllers;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.SnapshotParameters;
@@ -13,6 +14,7 @@ import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -65,12 +67,12 @@ public class GameHubController extends HomePageController{
                 new KeyFrame(Duration.seconds(0.2), e -> {
                     imageView.setImage(image);
                     gameLabel.setText("A game where one must take their knowledge to the test.");
-                    pane1.setStyle(
-                            "-fx-background-color: linear-gradient(to bottom, #02022E, #36108B, BLACK);"
-                    );
-                    anchorPane1.setStyle(
-                            "-fx-background-color: linear-gradient(to bottom, #02022E, #36108B, BLACK);"
-                    );
+//                    pane1.setStyle(
+//                            "-fx-background-color: linear-gradient(to bottom, #02022E, #36108B, BLACK);"
+//                    );
+//                    anchorPane1.setStyle(
+//                            "-fx-background-color: linear-gradient(to bottom, #02022E, #36108B, BLACK);"
+//                    );
                 }),
                 new KeyFrame(Duration.seconds(1), new KeyValue(imageView.opacityProperty(), 1.0))
         );
@@ -88,17 +90,41 @@ public class GameHubController extends HomePageController{
                 new KeyFrame(Duration.seconds(0.2), e -> {
                     imageView.setImage(image);
                     gameLabel.setText("Your challenge is to guess a five-letter word in six attempts.");
-                    pane1.setStyle(
-                            "-fx-background-color: linear-gradient(to bottom, #003C8C, #001D29, BLACK);"
-                    );
-                    anchorPane1.setStyle(
-                            "-fx-background-color: linear-gradient(to bottom, #003C8C, #001D29, BLACK);"
-                    );
+//                    pane1.setStyle(
+//                            "-fx-background-color: linear-gradient(to bottom, #003C8C, #001D29, BLACK);"
+//                    );
+//                    anchorPane1.setStyle(
+//                            "-fx-background-color: linear-gradient(to bottom, #003C8C, #001D29, BLACK);"
+//                    );
                 }),
                 new KeyFrame(Duration.seconds(1), new KeyValue(imageView.opacityProperty(), 1.0))
         );
         timeline.setCycleCount(1);
         timeline.play();
+    }
+
+    private final Duration animationDuration = Duration.millis(150); // 0.15 seconds
+
+    private void applyAnimation(TranslateTransition transition) {
+        transition.setCycleCount(1);
+        transition.setAutoReverse(false);
+        transition.play();
+    }
+
+    @FXML
+    public void inEffects3(MouseEvent event) {
+        TranslateTransition transition = new TranslateTransition(animationDuration, gotoGames);
+        transition.setToY(0);
+        applyAnimation(transition);
+        gotoGames.setStyle("-fx-background-color: #1A1A1A; -fx-text-fill: #fff; -fx-box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;");
+    }
+
+    @FXML
+    public void outEffects3(MouseEvent event) {
+        TranslateTransition transition = new TranslateTransition(animationDuration, gotoGames);
+        transition.setToY(0);
+        applyAnimation(transition);
+        gotoGames.setStyle("-fx-background-color: white; -fx-text-fill: #3B3B3B; -fx-box-shadow: none;");
     }
 
 }
