85a29838c415378bec956b518088c41296601ff9
Tenebris2 <buiducanh567@gmail.com>
2023-11-25 20:39:49 +0700

    added favorites

---
+++ src/main/java/com/example/englishlearningappv1/API/ChatBot.java
@@ -0,0 +1,102 @@
+package com.example.englishlearningappv1.API;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.google.gson.Gson;
+
+public class ChatBot {
+    private static final String endpoint = "https://api.openai.com/v1/chat/completions";
+    private static final String apiKey;
+
+    static {
+        try {
+            apiKey = API_KEY.getAPIKey("chat-gpt-api-key").getKey();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ChatBot() throws SQLException {
+    }
+
+    public static String sendQuery(String input) {
+        // Build input and API key params
+        JSONObject payload = new JSONObject();
+        JSONObject message = new JSONObject();
+        JSONArray messageList = new JSONArray();
+
+        message.put("role", "user");
+        message.put("content", input);
+        messageList.put(message);
+
+        payload.put("model", "gpt-3.5-turbo"); // model is important
+        payload.put("messages", messageList);
+        payload.put("temperature", 0.7);
+
+        StringEntity inputEntity = new StringEntity(payload.toString(), ContentType.APPLICATION_JSON);
+
+        // Build POST request
+        HttpPost post = new HttpPost(endpoint);
+        post.setEntity(inputEntity);
+        post.setHeader("Authorization", "Bearer " + apiKey);
+        post.setHeader("Content-Type", "application/json");
+
+        // Send POST request and parse response
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(post)) {
+            HttpEntity resEntity = response.getEntity();
+            String resJsonString = new String(resEntity.getContent().readAllBytes(), StandardCharsets.UTF_8);
+            JSONObject resJson = new JSONObject(resJsonString);
+
+            if (resJson.has("error")) {
+                String errorMsg = resJson.getString("error");
+
+                return "Error: " + errorMsg;
+            }
+
+            // Parse JSON response
+            JSONArray responseArray = resJson.getJSONArray("choices");
+            List<String> responseList = new ArrayList<>();
+
+            for (int i = 0; i < responseArray.length(); i++) {
+                JSONObject responseObj = responseArray.getJSONObject(i);
+                String responseString = responseObj.getJSONObject("message").getString("content");
+                responseList.add(responseString);
+            }
+
+            // Convert response list to JSON and return it
+            Gson gson = new Gson();
+            String jsonResponse = gson.toJson(responseList);
+            Pattern pattern = Pattern.compile("\"([^\"]*)\"");
+            Matcher matcher = pattern.matcher(jsonResponse);
+            String extractedString = "";
+            if (matcher.find()) {
+                extractedString = matcher.group(1);
+            }
+
+            return extractedString;
+        } catch (IOException | JSONException e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(ChatBot.sendQuery("tell me the definition of the word good, make it short"));
+    }
+}
